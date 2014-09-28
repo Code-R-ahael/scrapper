d33a318a06c5778ab51388b9bad7b67a5c94be48
Code-R-ahael <rahael.saldanha@gmail.com>
2014-09-28 11:44:41 +0530

    attendance scraper commit

--- scrapper3.java
+++ scrapper3.java
@@ -24,13 +24,6 @@ public class scrapper3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try{
-		/*Document doc = Jsoup.connect("http://websismit.manipal.edu/websis/control/createAnonSession")
-							.data("idValue", "120905110", "birthDate_i18n", "1994-02-05", "birthDate", "1994-02-05")
-							.cookie("auth", "token")
-							.timeout(9000)
-				            .ignoreContentType(true)
-				            .userAgent("Google Chrome")
-				            .post();*/
 			Map<String, String> cookies = new HashMap<String, String>();
 			Connection.Response res = Jsoup.connect("http://websismit.manipal.edu/websis/control/createAnonSession")
 				    .data("idValue", "120905110", "birthDate", "1994-02-05")
@@ -44,11 +37,12 @@ public class scrapper3 {
 					    .ignoreContentType(true)
 					    .timeout(11000)
 					    .post();
-				for (Entry<String, String> cookie : cookies.entrySet()) {
+				//System.out.println(doc.toString());
+				/*for (Entry<String, String> cookie : cookies.entrySet()) {
 				    res.cookie(cookie.getKey(), cookie.getValue());
 				    //System.out.println(cookie.getKey()+cookie.getValue());
-				}
-				Elements links = doc.select("a[href]");
+				}*/
+				/*Elements links = doc.select("a[href]");
 				String acSec="";
 		        for (Element link : links) {
 		        	if(link.attr("href").contains("Academic"))
@@ -57,24 +51,24 @@ public class scrapper3 {
 		        //System.out.println("\nlink : " + link.attr("href"));
 
 		        }
-		       
+		        //System.out.println(acSec);
 		        String acLk="http://websismit.manipal.edu/websis/control"+acSec;
-		        //System.out.println(acLk);
-		        Connection.Response res2 = Jsoup.connect(acLk)
-					    .execute();
-		        
-		        cookies.putAll(res2.cookies());
-		        Document acdoc = res2.parse();
-					    /*.ignoreContentType(true)
-					    .timeout(11000)
-					    .post();*/
-		        System.out.println(acdoc.toString());
-				/*Element tile =doc.select("div.tile").first();
-				Element s =tile.select("table.basic-table").first();
-				String add=s.getElementsByTag("div").eq(1).text();
-				String eid=s.getElementsByTag("div").eq(3).text();
-				System.out.println(add);
-				System.out.println(eid);*/
+		        //System.out.println(acLk);*/
+		        cookies=res.cookies();
+		      
+		        Document acdoc = Jsoup.connect("http://websismit.manipal.edu/websis/control/StudentAcademicProfile")
+		        		.cookie("JSESSIONID",cookies.get("JSESSIONID"))
+		        		.cookie("OFBiz.Visitor", cookies.get("OFBiz.Visitor"))
+		        		.timeout(6000)
+		        		.get();
+				Element tile =acdoc.select("div.screenlet-body").get(1);
+				Element s =tile.getElementById("ListAttendanceSummary_table");
+				String sub[]=new String[6];
+				for(int i=0;i<6;i++)
+				{
+					sub[i]=s.getElementsByTag("tr").eq(i+1).text();
+					System.out.println(sub[i]);
+				}
 		}
 		catch(Exception e)
 		{
